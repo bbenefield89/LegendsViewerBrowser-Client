e26ee4a99b7de901d6402eed781e4fa33560b6b9
bbenefield89 <bsquared18@gmail.com>
2019-03-24 17:38:19 -0400

    ADD PROPS BEING PASSED DOWN
    - src/components/LegendsList/LegendsList.vue

---
+++ src/components/LegendsList/LegendsList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>{{ listTitle }}</h2>
+    
+    <ul>
+      <li v-bind:key="item.id[0]" v-for="item of list">
+        <span v-if="item.description">{{ item.description[0] }}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component({
+  name: 'LegendsList'
+})
+class LegendsList extends Vue {
+  @Prop() public listTitle!: string
+  @Prop() public list!: any
+}
+
+export default LegendsList
+</script>
+
+<style lang="scss" scoped>
+</style>
