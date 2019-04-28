dbc4e4481a4cdb5dcb6435e86fe3deb4e12baeaa
Brandon Benefield <bsquared18@gmail.com>
2019-04-27 23:10:56 -0400

    CHANGE FORMS SUBMIT LOGIC TO EMIT AN EVENT
    - src/components/UploadLegendsForm/UploadLegendsForm.vue

--- src/components/UploadLegendsForm/UploadLegendsForm.vue
+++ src/components/UploadLegendsForm/UploadLegendsForm.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <form v-on:submit="uploadLegendsXmlFile">
+    <form @submit="uploadLegendsXmlFile">
       <label for="upload_legends_input" />
-      <input name="upload_legends_input" type="file" />
+      <input name="upload_legends_input" type="file" accept=".xml" />
       <input type="submit" />
     </form>
 
-    <HistoricalFigures :historical_figures="historical_figures" />
+    <!-- <HistoricalFigures :historical_figures="historical_figures" /> -->
     <!-- <DanceForms :dance_forms="dance_forms" />
     <Entities :entities="entities" />
     <EntityPopulations :entity_populations="entity_populations" />
@@ -41,7 +41,21 @@ import UndergroundRegions from '../LegendsList/UndergroundRegions/UndergroundReg
 
 @Component({
   name: 'UploadLegendsForm',
-  components: { LegendsList, DanceForms, Entities, EntityPopulations, HistoricalEras, HistoricalEventCollections, HistoricalEvents, HistoricalFigures, MusicalForms, PoeticForms, Regions, Sites, UndergroundRegions }
+  components: {
+    DanceForms,
+    Entities,
+    EntityPopulations,
+    HistoricalEras,
+    HistoricalEventCollections,
+    HistoricalEvents,
+    HistoricalFigures,
+    LegendsList,
+    MusicalForms,
+    PoeticForms,
+    Regions,
+    Sites,
+    UndergroundRegions
+  }
 })
 export default class UploadLegendsForm extends Vue {
   public dance_forms: object[] = []
@@ -67,26 +81,37 @@ export default class UploadLegendsForm extends Vue {
     fileReader.onprogress = (res: any): any => null
     fileReader.onload = (res: any): void => {
       parseString(res.srcElement.result, (err: any, result: any) => {
-        console.log(result.df_world)
-        this.dance_forms = result.df_world.dance_forms[0].dance_form
-        this.entities = result.df_world.entities[0].entity
-        this.entity_populations = result.df_world.entity_populations[0].entity_population
-        this.historical_eras = result.df_world.historical_eras[0].historical_era
-        this.historical_event_collections = result.df_world.historical_event_collections[0].historical_event_collection
-        this.historical_events = result.df_world.historical_events[0].historical_event
-        this.historical_figures = result.df_world.historical_figures[0].historical_figure
-        this.musical_forms = result.df_world.musical_forms[0].musical_form
-        this.poetic_forms = result.df_world.poetic_forms[0].poetic_form
-        this.regions = result.df_world.regions[0].region
-        this.sites = result.df_world.sites[0].site
-        this.underground_regions = result.df_world.underground_regions[0].underground_region
+        // console.log(result.df_world)
+        const dwarfFortressWorldData = {
+          DanceForms: result.df_world.dance_forms[0].dance_form,
+          Entities: result.df_world.entities[0].entity,
+          EntityPopulations: result.df_world.entity_populations[0].entity_population,
+          HistoricalEras: result.df_world.historical_eras[0].historical_era,
+          HistoricalEventCollections: result.df_world.historical_event_collections[0].historical_event_collection,
+          HistoricalEvents: result.df_world.historical_events[0].historical_event,
+          HistoricalFigures: result.df_world.historical_figures[0].historical_figure,
+          MusicalForms: result.df_world.musical_forms[0].musical_form,
+          PoeticForms: result.df_world.poetic_forms[0].poetic_form,
+          Regions: result.df_world.regions[0].region,
+          Sites: result.df_world.sites[0].site,
+          UndergroundRegions: result.df_world.underground_regions[0].underground_region
+        }
+        this.$emit('upload-legends-file', dwarfFortressWorldData)
+        // this.dance_forms = result.df_world.dance_forms[0].dance_form
+        // this.entities = result.df_world.entities[0].entity
+        // this.entity_populations = result.df_world.entity_populations[0].entity_population
+        // this.historical_eras = result.df_world.historical_eras[0].historical_era
+        // this.historical_event_collections = result.df_world.historical_event_collections[0].historical_event_collection
+        // this.historical_events = result.df_world.historical_events[0].historical_event
+        // this.historical_figures = result.df_world.historical_figures[0].historical_figure
+        // this.musical_forms = result.df_world.musical_forms[0].musical_form
+        // this.poetic_forms = result.df_world.poetic_forms[0].poetic_form
+        // this.regions = result.df_world.regions[0].region
+        // this.sites = result.df_world.sites[0].site
+        // this.underground_regions = result.df_world.underground_regions[0].underground_region
       })
     }
     fileReader.readAsText(legendsXmlFile)
-  }
-
-  public filterList(): any {
-    console.log('Hello')
   }
 }
 </script>
