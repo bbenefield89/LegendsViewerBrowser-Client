f61dcd70391f261e7b2e3fa6acb734c8b5f8c0db
Brandon Benefield <bsquared18@gmail.com>
2019-04-28 21:02:38 -0400

    ADD THE REST OF THE DATA THAT SHOULD BE RENDERED WITH <HistoricalEvents />
    - src/components/LegendsList/HistoricalEvents/HistoricalEvents.vue
      - this component is not being used as of right now

--- src/components/LegendsList/HistoricalEvents/HistoricalEvents.vue
+++ src/components/LegendsList/HistoricalEvents/HistoricalEvents.vue
@@ -2,8 +2,17 @@
   <div>
     <h2>Historical Events</h2>
     <ul>
-      <li v-bind:key="historical_event.id[0]" v-for="historical_event in historical_events">
-        <p>{{ historical_event.id[0] }}</p>
+      <li v-bind:key="historical_event.id[0]" v-for="historical_event in data">
+        <p>ID: {{ historical_event.id[0] }}</p>
+        <p v-if="historical_event.hasOwnProperty('coords')">Coords: {{ historical_event.coords[0] }}</p>
+        <p v-if="historical_event.hasOwnProperty('feature_layer_id')">Feature Layer ID: {{ historical_event.feature_layer_id[0] }}</p>
+        <p v-if="historical_event.hasOwnProperty('hfid')">Historical Figure ID: {{ historical_event.hfid[0] }}</p>
+        <p v-if="historical_event.hasOwnProperty('hfid')">Historical Figure ID: {{ historical_event.hfid[0] }}</p>
+        <p v-if="historical_event.hasOwnProperty('site_id')">Site ID: {{ historical_event.site_id[0] }}</p>
+        <p v-if="historical_event.hasOwnProperty('state')">State: {{ historical_event.state[0] }}</p>
+        <p v-if="historical_event.hasOwnProperty('subregion_id')">Subregion ID: {{ historical_event.subregion_id[0] }}</p>
+        <p>Type: {{ historical_event.type[0] }}</p>
+        <p>Year: {{ historical_event.year[0] }}</p>
       </li>
     </ul>
   </div>
@@ -16,7 +25,7 @@ import { Component, Prop, Vue } from 'vue-property-decorator'
   name: 'HistoricalEvents'
 })
 class HistoricalEvents extends Vue {
-  @Prop() public historical_events!: object[]
+  @Prop() public data!: object[]
 }
 
 export default HistoricalEvents
