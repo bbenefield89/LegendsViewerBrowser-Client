69e950abf60d560e376000ad48279a5d1812070b
Brandon Benefield <bsquared18@gmail.com>
2019-04-28 21:04:55 -0400

    ADD CONDITIONAL RENDERING DEPENDING ON THE TYPE OF DATA BEING RENDERED
    - src/components/LegendsList/HistoricalFigures/HistoricalFigureDataItem.vue
      - some data in the HistoricalFigures object is an array of objects or a string and need to be handled differently depending on what should be shown on the UI

--- src/components/LegendsList/HistoricalFigures/HistoricalFigureDataItem.vue
+++ src/components/LegendsList/HistoricalFigures/HistoricalFigureDataItem.vue
@@ -5,12 +5,28 @@
     </h3>
 
     <!-- IF -->
-    <!-- <ul v-if="title.toLowerCase() === 'skills'"> -->
     <ul v-if="hfData[0].constructor.name === 'Object'">
       <li v-for="(data, idx) in hfData" :key="idx">
-        <p v-for="val of data" :key="val[0]">
-          {{ val[0] }}
-        </p>
+        <!-- IF -->
+        <template v-if="title === 'Entity Link'">
+          <ul v-for="val in data" :key="val[0]">
+            <!-- IF -->
+            <li v-if="Number(val[0]) >= 0">
+              <p>Entity ID: {{ val[0] }}</p>
+              <p>Entity Name: {{ getEntityLinkName(val) }}</p>
+            </li>
+            <!-- ELSE -->
+            <li v-else>
+              <p>{{ val[0] }}</p>
+            </li>
+          </ul>
+        </template>
+        <!-- ELSE -->
+        <template v-else>
+          <p v-for="val of data" :key="val[0]">
+            {{ val[0] }}
+          </p>
+        </template>
       </li>
     </ul>
     <!-- ELSE -->
@@ -32,6 +48,7 @@ class HistoricalFigureDataItem extends Vue {
 
   @Prop() public hfData!: any
   @Prop() public title!: string
+  @Prop() public dfWorldHistory?: any
   
   public debug() {
     debugger
@@ -39,6 +56,13 @@ class HistoricalFigureDataItem extends Vue {
 
   public getObjectEntries(obj: object): Array<any[]> {
     return Object.entries(obj)
+  }
+
+  public getEntityLinkName(current: any): any {
+    const entityLinkName: any = this.dfWorldHistory.Entities.filter((entity: any) => {
+      return entity.id[0] === current[0]
+    })[0]
+    return entityLinkName === undefined ? 'UNKNOWN' : entityLinkName.name ? entityLinkName.name[0] : 'UNKNOWN'
   }
   
   public logger(data: any): void {
